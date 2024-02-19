75bcc8e1e00f4d40af74e48cf3d2e4092ef815a4
Jerry Lyu <45759373+astro2049@users.noreply.github.com>
2024-02-19 17:29:58 +0000

    1 Game Metrics

---
+++ src/main/java/games/jaipurskeleton/stats/JaipurMetrics.java
@@ -0,0 +1,89 @@
+package games.jaipurskeleton.stats;
+
+import core.actions.AbstractAction;
+import core.interfaces.IGameEvent;
+import evaluation.listeners.MetricsGameListener;
+import evaluation.metrics.AbstractMetric;
+import evaluation.metrics.Event;
+import evaluation.metrics.IMetricsCollection;
+import games.jaipurskeleton.JaipurGameState;
+import games.jaipurskeleton.actions.TakeCards;
+import games.jaipurskeleton.components.JaipurCard;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class JaipurMetrics implements IMetricsCollection {
+    static public class RoundScoreDifference extends AbstractMetric {
+
+        @Override
+        protected boolean _run(MetricsGameListener listener, Event e, Map<String, Object> records) {
+            JaipurGameState gs = (JaipurGameState) e.state;
+            double scoreDiff = 0;
+            for (int i = 0; i < gs.getNPlayers() - 1; i++) {
+                scoreDiff += Math.abs(gs.getPlayerScores().get(i).getValue() - gs.getPlayerScores().get(i + 1).getValue());
+            }
+            scoreDiff /= (gs.getNPlayers() - 1);
+            records.put("ScoreDiff", scoreDiff);
+            return true;
+        }
+
+        @Override
+        public Set<IGameEvent> getDefaultEventTypes() {
+            return Collections.singleton(Event.GameEvent.ROUND_OVER);
+        }
+
+        @Override
+        public Map<String, Class<?>> getColumns(int nPlayersPerGame, Set<String> playerNames) {
+            Map<String, Class<?>> columns = new HashMap<>();
+            columns.put("ScoreDiff", Double.class);
+            return columns;
+        }
+    }
+
+    static public class PurchaseFromMarket extends AbstractMetric {
+        JaipurCard.GoodType[] goodTypes;
+
+        public PurchaseFromMarket() {
+            super();
+            goodTypes = JaipurCard.GoodType.values();
+        }
+
+        public PurchaseFromMarket(String[] args) {
+            super(args);
+            goodTypes = new JaipurCard.GoodType[args.length];
+            for (int i = 0; i < args.length; i++) {
+                goodTypes[i] = JaipurCard.GoodType.valueOf(args[i]);
+            }
+        }
+
+        @Override
+        protected boolean _run(MetricsGameListener listener, Event e, Map<String, Object> records) {
+            AbstractAction action = e.action;
+            if (action instanceof TakeCards tc) {
+                for (JaipurCard.GoodType type : goodTypes) {
+                    if (tc.howManyPerTypeTakeFromMarket.containsKey(type))
+                        records.put("Purchase - " + type.name(), tc.howManyPerTypeTakeFromMarket.get(type));
+                }
+                return true;
+            }
+            return false;
+        }
+
+        @Override
+        public Set<IGameEvent> getDefaultEventTypes() {
+            return Collections.singleton(Event.GameEvent.ACTION_CHOSEN);
+        }
+
+        @Override
+        public Map<String, Class<?>> getColumns(int nPlayersPerGame, Set<String> playerNames) {
+            Map<String, Class<?>> columns = new HashMap<>();
+            for (JaipurCard.GoodType type : goodTypes) {
+                columns.put("Purchase - " + type.name(), Integer.class);
+            }
+            return columns;
+        }
+    }
+}
